9224ebaff61f1eaeeb067b4938264b6a03825dea
THEElianaPRO <theelianapro@gmail.com>
2021-08-18 00:58:03 -0700

    first commit

---
+++ THEElianaPROBot/modules/__tools.py
@@ -0,0 +1,22 @@
+__mod_name__ = "◎Tools"
+
+__help__ = """
+*QR code*
+ ❍ /getqr*:* Get text in QR
+ ❍ /makeqr*:* Make a QR code.
+
+*Date-time-Weather*
+ ❍ /time <country code>*:* Gives information about a timezone.
+ ❍ /weather <city>*:* Get weather info in a particular place.
+ ❍ /wttr <city>*:* Advanced weather module, usage same as /weather
+ ❍ /wttr moon*:* Get the current status of moon
+ 
+*Quotly*
+- /q : reply any message with this command to create a sticker.
+
+*Converts*
+ ❍ /encrypt*:* Encrypts The Given Text
+ ❍ /decrypt*:* Decrypts Previously Ecrypted Text
+ ❍ /zip*:* reply to a telegram file to compress it in .zip format
+ ❍ /unzip*:* reply to a telegram file to decompress it from the .zip format
+"""
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE THE Eliana PRO Bot
+:: Enables virtual env mode and then starts Rika
+env\scripts\activate.bat && py -m THEElianaPROBot
