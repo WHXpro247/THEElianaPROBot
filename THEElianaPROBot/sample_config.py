a84bb399c099cda98aed9c3d7d16d953e2e856eb
THEElianaPRO <88980596+THEElianaPRO@users.noreply.github.com>
2021-08-26 17:47:57 +0800

    Update sample_config.py

--- THEElianaPROBot/sample_config.py
+++ THEElianaPROBot/sample_config.py
@@ -1,70 +1,67 @@
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
+from os import getenv
 
 
 def get_user_list(config, key):
-    with open("{}/THEElianaPROBot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/LaylaRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 7284929  # integer value, dont use ""
-    API_HASH = "c69b39a9cff8c485a00d1a7a0d81853f"
-    TOKEN = "1989297177:AAGMV9ajivUxXbV0_KTPwhsrZzctpgNR0ms"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1840378738  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Eliana2020Eliana" 
-    BOT_ID = 1989297177
-    SUPPORT_CHAT = "TEP_Discussion"  # Your own group for support, do not add the @
-    JOIN_LOGGER = (
-        -1001350838747
-    )  # Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = (
-        -1001504119947
-    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    API_ID = 1367591  # integer value, dont use ""
+    API_HASH = "5d4ebdf8b500f26507cc31d2857ff668"
+    TOKEN = "1885209485:AAEvzi2LXCB91XLEcFZkmOYtBjK7_RMb2PM"  # Your Bot Token
+    OWNER_ID = 797768146  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "HEROGAMERS1" 
+    BOT_ID = ""
+    HEROKU_APP_NAME = ""
+    SUPPORT_CHAT = "AwesomeSupport"  # Your own group for support, do not add the @
+    JOIN_LOGGER = (-1001392103597)
+    EVENT_LOGS = (-1001392103597)  # Logs Channel Id do not add the @
+    DATABASE_URL = getenv("DATABASE_URL", "")
+    MONGO_DB_URI = "mongodb+srv://hero:hero@cluster0.2nyzp.mongodb.net/test?retryWrites=true&w=majority"
+    HEROKU_API_KEY = getenv("HEROKU_API_KEY", "")
+    DONATION_LINK = getenv("DONATION_LINK", "https://t.me/LaylaList")
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
-    MONGO_DB_URI = "mongodb+srv://hero9711:hero9711@cluster0.isykk.mongodb.net/test?retryWrites=true&w=majority"
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "TEP_Discussion"
-
-    # OPTIONAL
-    ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "sudos")
-    ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "devs")
-    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = get_user_list("elevated_users.json", "supports")
-    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "tigers")
-    WOLVES = get_user_list("elevated_users.json", "whitelists")
-    DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
-    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
+    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    SPAMWATCH_API = ""
+    SPAMWATCH_SUPPORT_CHAT = "SpamWatchSupport"
+    
+
+    # OPTIONAL
+    ##List of id's -  (not usernames) 
+    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    DEMONS = get_user_list("elevated_users.json", "supports")
+    TIGERS = get_user_list("elevated_users.json", "tigers")
+    WOLVES = get_user_list("elevated_users.json", "whitelists")
     WORKERS = (
         8  # Number of subthreads to use. Set as number of threads your processor uses
     )
     BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
-    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
-    )
-    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
+        "WVUTR9R34VAM5LE0"  # Get your API key from https://www.alphavantage.co/support/#api-key 
+    )  
+    TIME_API_KEY = "4LTJHD1G5ZYD" # Get your API key from https://timezonedb.com/api
     WALL_API = (
-        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
+        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php 
     )
     AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
