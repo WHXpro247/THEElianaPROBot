3f12db8be387fdefedb8edc9134f55d512cd7ffa
THEElianaPRO <88980596+THEElianaPRO@users.noreply.github.com>
2021-08-26 18:07:07 +0800

    Update and rename sample_config.py to config.py

--- THEElianaPROBot/config.py
+++ THEElianaPROBot/config.py
@@ -16,7 +16,7 @@ class Config(object):
 
     API_ID = 1367591  # integer value, dont use ""
     API_HASH = "5d4ebdf8b500f26507cc31d2857ff668"
-    TOKEN = "1885209485:AAEvzi2LXCB91XLEcFZkmOYtBjK7_RMb2PM"  # Your Bot Token
+    TOKEN = "1989799118:AAHXLVtLl9WwIkyqA24NXUbDcTQVEPf3KzI"  # Your Bot Token
     OWNER_ID = 797768146  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "HEROGAMERS1" 
     BOT_ID = ""
